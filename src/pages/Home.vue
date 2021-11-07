dcd954a3e4f846e54a3af2efc2187abce92d0f6c
ssuyun.chen <ssuyun.chen@udngroup.com>
2021-11-07 15:32:31 +0800

    test

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -5,15 +5,15 @@
     <div class="main">
       <div class="main__wrap">
         <h2 class="main__title main__title--1">必遊景點</h2>
-        <img src="../assets/img/home_scene.jpg" alt="scene">
+        <img src="../assets/img/scene.jpg" alt="scene">
       </div>
       <div class="main__wrap">
         <h2 class="main__title main__title--2">特色美食</h2>
-        <img src="../assets/img/home_food.jpg" alt="food">
+        <img src="../assets/img/food.jpg" alt="food">
       </div>
       <div class="main__wrap">
         <h2 class="main__title main__title--3">住宿特搜</h2>
-        <img src="../assets/img/home_room.jpg" alt="room">
+        <img src="../assets/img/room.jpg" alt="room">
       </div>
     </div>
   </section>
