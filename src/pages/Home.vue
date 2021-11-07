d416af7e02c71d7b714103e6a494d28dea5606ca
ssuyun.chen <ssuyun.chen@udngroup.com>
2021-11-07 14:45:17 +0800

    upadte img

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,19 +1,19 @@
 <template>
   <section class="home">
-    <img src="../assets/img/banner.jpg" alt="banner">
+    <img src="@/assets/img/banner.jpg" alt="banner">
     <h1>帶您探索台灣之美</h1>
     <div class="main">
       <div class="main__wrap">
         <h2 class="main__title main__title--1">必遊景點</h2>
-        <img src="../assets/img/home_scene.jpg" alt="scene">
+        <img src="@/assets/img/home_scene.jpg" alt="scene">
       </div>
       <div class="main__wrap">
         <h2 class="main__title main__title--2">特色美食</h2>
-        <img src="../assets/img/home_food.jpg" alt="food">
+        <img src="@/assets/img/home_food.jpg" alt="food">
       </div>
       <div class="main__wrap">
         <h2 class="main__title main__title--3">住宿特搜</h2>
-        <img src="../assets/img/home_room.jpg" alt="room">
+        <img src="@/assets/img/home_room.jpg" alt="room">
       </div>
     </div>
   </section>
